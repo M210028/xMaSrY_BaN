7abf944f84d275fd68eafd32c766a6d78715bb23
マスリー <ahemdsh904@gmail.com>
2026-05-28 08:48:33 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ setup(
     name="xMaSrY_BaN",
     version="1.0.1",
     packages=find_packages(),
+    py_modules=["xMaSrY_BaN"],
     install_requires=[
         "requests",
         "pycryptodome",
         "protobuf",
         "cachetools"
     ],
+    author="xMaSrY",
+    description="Python utility library",
+    python_requires=">=3.9",
 )
